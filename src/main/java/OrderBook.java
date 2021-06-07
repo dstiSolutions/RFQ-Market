5daeee7d4d09d6dd65bff24aa54731f048a6cd0f
SpinFoam <64492048+Diffeomorph@users.noreply.github.com>
2021-06-07 20:36:49 +0100

    changed creation of orderbook

--- src/main/java/OrderBook.java
+++ src/main/java/OrderBook.java
@@ -4,8 +4,9 @@ public class OrderBook {
     private HashMap<Integer, Integer> buyPrices= new HashMap(); //from price level to quantity
     private HashMap<Integer, Integer> sellPrices= new HashMap();
 
-    OrderBook(){
+    OrderBook(Double midPrice){
         reset();
+        generateOrderBook(midPrice);
     }
 
     public void reset(){
@@ -15,14 +16,14 @@ public class OrderBook {
 
     public void generateOrderBook(Double midPrice){
         int i = 0;
-        int startSell = (int)Math.ceil(midPrice);
+        int startSell = (int) Math.ceil(midPrice);
         while (i < 10){
             sellPrices.put(i+startSell,100+i*100);
             i++;
         }
 
         int j = 0;
-        int startBuy = (int)Math.floor(midPrice);
+        int startBuy = (int) Math.floor(midPrice);
         while (j < 10){
             sellPrices.put(startBuy-j,100+j*100);
             j++;
